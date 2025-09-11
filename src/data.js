7e1d9e910dfa3b8c3954a5d605590d3a8715bb8f
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-11 06:55:37 -0500

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const questions = [
+  {
+    question: "¿Cuál es el planeta más grande del sistema solar?",
+    options: ["Tierra", "Júpiter", "Marte", "Saturno"],
+    answer: "Júpiter",
+  },
+  {
+    question: "¿Quién pintó la Mona Lisa?",
+    options: ["Van Gogh", "Picasso", "Da Vinci", "Rembrandt"],
+    answer: "Da Vinci",
+  },
+  {
+    question: "¿Qué lenguaje se usa en React?",
+    options: ["Python", "Java", "JavaScript", "C#"],
+    answer: "JavaScript",
+  },
+];
