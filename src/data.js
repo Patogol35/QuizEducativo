c3c36fbbc8d67fbb2d4c3e5f147a7cfaa82e2e2b
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-09-11 20:21:14 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -50,7 +50,7 @@ export const questions = [
   { question: "¿Qué volcán se encuentra cerca de Ambato?", options: ["Cotopaxi", "Chimborazo", "Pichincha", "Tungurahua"], answer: "Tungurahua" },
   { question: "¿Qué río pasa por Ambato?", options: ["Río Guayas", "Río Ambato", "Río Pastaza", "Río Esmeraldas"], answer: "Río Ambato" },
   { question: "¿Cuál es el plato típico de Ambato?", options: ["Fanesca", "Llapingacho", "Cuy asado", "Humitas"], answer: "Llapingacho" },
-  { question: "¿Qué fiesta es famosa en Ambato?", options: ["Fiesta de la Mama Negra", "Fiesta de las frutas y de las flores", "Inti Raymi", "Feria de Quito"], answer: "Fiesta de las frutas y de las flores" },
+  { question: "¿Qué fiesta es famosa en Ambato?", options: ["Fiesta de Guayaquil", "Fiesta de las frutas y de las flores", "Inti Raymi", "Feria de Quito"], answer: "Fiesta de las frutas y de las flores" },
   { question: "¿En qué provincia está ubicada Ambato?", options: ["Pichincha", "Tungurahua", "Cotopaxi", "Imbabura"], answer: "Tungurahua" },
   { question: "¿Qué parque es famoso en Ambato?", options: ["Parque Cevallos", "Parque La Carolina", "Parque Metropolitano", "Parque El Ejido"], answer: "Parque Cevallos" },
   { question: "¿Qué fruta es muy representativa de Ambato?", options: ["Banano", "Manzana", "Naranja", "Piña"], answer: "Manzana" },
